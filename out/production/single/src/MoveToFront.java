0d2cc97e8dd94016d2f1b312d0042b13dd73d151
Meleny <865622793@qq.com>
2020-03-03 14:07:01 +0800

    the CircularSuffixArray is done

--- out/production/single/src/MoveToFront.java
+++ out/production/single/src/MoveToFront.java
@@ -1,61 +1,53 @@
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
-import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
 
 public class MoveToFront {
 
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
         //init
-        char[] seq = new char[256];
-        for (char i = 0; i < 256; i++) {
-            seq[i] = i;
-
-//            System.out.println(seq[i]);
+        ArrayList<Character> list = new ArrayList<>();
+        for (char c = 0; c < 256; c++) {
+            list.add(c);
         }
 
-
-        //read and print and move
+        //handle
         while (!BinaryStdIn.isEmpty()) {
-            char ch = BinaryStdIn.readChar();
-            System.out.println("the ch is: " + ch);
-            //find the position of ch
-            char position = 0;
-            if (seq[ch] == ch) {
-                position = ch;
-            } else {
-                for (char i = 0; i < 256; i++) {
-                    if (seq[i] == ch) {
-                        position = i;
-                        break;
-                    }
-                }
-            }
-//            BinaryStdOut.write(position);
-            System.out.println("position = " + Integer.toHexString(position));
+            char c = BinaryStdIn.readChar();
 
+            int index = list.indexOf(c);
+            BinaryStdOut.write(index, 8);
 
-            //move to the front; bubble up
-            if (position != 0) {
-                while (position != 0) {
-                    swapChar(seq, position, position - 1);
-                    position--;
-                }
-            }
+            list.remove(index);
+            list.add(0, c);
         }
 
+        //close the stream
+        BinaryStdOut.close();
     }
-
-    private static void swapChar(char[] seq, char pos1, int pos2) {
-        char temp = seq[pos1];
-        seq[pos1] = seq[pos2];
-        seq[pos2] = temp;
-    }
-
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode() {
+        //init
+        ArrayList<Character> list = new ArrayList<>();
+        for (char c = 0; c < 256; c++) {
+            list.add(c);
+        }
 
+        //handle
+        while (!BinaryStdIn.isEmpty()) {
+            int index = BinaryStdIn.readInt(8);
+            char ch = list.get(index);
+            BinaryStdOut.write(ch);
+
+            list.remove(index);
+            list.add(0, ch);
+        }
+
+        //close the stream
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply move-to-front encoding
--- src/CircularSuffixArray.java
+++ src/CircularSuffixArray.java
@@ -1,24 +1,57 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class CircularSuffixArray {
-    private final String string;
+    private String input;
+    private Integer[] index;
 
     // circular suffix array of s
-    public CircularSuffixArray(String s){
-        string=s;
+    public CircularSuffixArray(String s) {
+        if (s == null) throw new IllegalArgumentException();
+
+        int len = s.length();
+        index = new Integer[len];
+
+        // init
+        for (int i = 0; i < len; i++) index[i] = i;
+
+        // sort
+        Arrays.sort(index, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer index1, Integer index2) {
+                // originalArray[index] start from index
+                for (int i = 0; i < len; i++) {
+                    if (index1 >= len) index1 = 0;
+                    if (index2 >= len) index2 = 0;
+
+                    if (s.charAt(index1) > s.charAt(index2)) return 1;
+                    if (s.charAt(index1) < s.charAt(index2)) return -1;
+
+                    index1++;
+                    index2++;
+                }
+                return 0;
+            }
+        });
     }
 
     // length of s
-    public int length(){
-        return string.length();
+    public int length() {
+        return index.length;
     }
 
     // returns index of ith sorted suffix
-    public int index(int i){
-        return 0;
+    public int index(int i) {
+        if (i < 0 || i >= length()) throw new IllegalArgumentException();
+        return index[i];
     }
 
     // unit testing (required)
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+//        CircularSuffixArray circularSuffixArray = new CircularSuffixArray("ABRACADABRA!");
+//        for (int i = 0; i < circularSuffixArray.length(); i++) {
+//            System.out.println(circularSuffixArray.index(i));
+//        }
     }
 
 }
